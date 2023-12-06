b4b3f654a809b3db2b8a5aff6c000d133b8fa53f
Xander Hendry <vhendry.coding@gmail.com>
2023-12-05 20:39:23 -0500

    add Market model and spec test, validations only for now

--- app/models/market.rb
+++ app/models/market.rb
@@ -1,2 +1,3 @@
 class Market < ApplicationRecord
+  validates :name, :street, :city, :county, :state, :zip, :lat, :lon, presence: true
 end
--- spec/models/market_spec.rb
+++ spec/models/market_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Market, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:street) }
+    it { should validate_presence_of(:city) }
+    it { should validate_presence_of(:county) }
+    it { should validate_presence_of(:state) }
+    it { should validate_presence_of(:zip) }
+    it { should validate_presence_of(:lat) }
+    it { should validate_presence_of(:lon) }
+  end
 end
