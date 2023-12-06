cecd9e2c02c5ec426d9d7a87d1d851f112a67167
Xander Hendry <vhendry.coding@gmail.com>
2023-12-05 20:55:46 -0500

    add market show endpoint, and spec test, remove unused actions from market controller

--- app/controllers/api/v0/markets_controller.rb
+++ app/controllers/api/v0/markets_controller.rb
@@ -6,25 +6,7 @@ module Api
       end
 
       def show
-        render json: market.find(params[:id])
-      end
-
-      def create
-        render json: market.create(market_params)
-      end
-
-      def update
-        render json: market.update(params[:id], market_params)
-      end
-
-      def destroy
-        render json: market.delete(params[:id])
-      end
-
-      private
-
-      def market_params
-        params.require(:market).permit(:name, :street, :city, :county, :state, :zip, :lat, :lon)
+        render json: Market.find(params[:id])
       end
     end
   end
--- spec/requests/api/v0/markets_request_spec.rb
+++ spec/requests/api/v0/markets_request_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper' 
 
 RSpec.describe "Markets Endpoints" do
-  describe 'get all markets' do 
+  describe 'Markets Index endpoint (/api/v0/markets)' do 
       it 'sends a list of all markets' do 
         create_list(:market, 3)
         
@@ -44,4 +44,42 @@ RSpec.describe "Markets Endpoints" do
       end
     end 
   end 
+  describe 'Market Show endpoint (/api/v0/markets/:id)' do 
+    it "can get one market by its id" do
+      id = create(:market).id
+    
+      get "/api/v0/markets/#{id}"
+    
+      market = JSON.parse(response.body, symbolize_names: true)
+    
+      expect(response).to be_successful
+    
+      expect(market).to have_key(:id)
+      expect(market[:id]).to be_an(Integer)
+
+      expect(market).to have_key(:name)
+      expect(market[:name]).to be_a(String)
+
+      expect(market).to have_key(:street)
+      expect(market[:street]).to be_a(String)
+
+      expect(market).to have_key(:city)
+      expect(market[:city]).to be_a(String)
+
+      expect(market).to have_key(:county)
+      expect(market[:county]).to be_a(String)
+
+      expect(market).to have_key(:state)
+      expect(market[:state]).to be_a(String)
+
+      expect(market).to have_key(:zip)
+      expect(market[:zip]).to be_a(String)
+
+      expect(market).to have_key(:lat)
+      expect(market[:lat]).to be_a(String)
+
+      expect(market).to have_key(:lon)
+      expect(market[:lon]).to be_a(String)
+    end
+  end
 end
