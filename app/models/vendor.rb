56cae6f5768fbd404df26e9b09efed760c09e942
Xander Hendry <vhendry.coding@gmail.com>
2023-12-06 16:54:30 -0500

    add custom validation for boolean value of :credit_accepted field for Vendors, including spec test

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,7 +1,13 @@
 class Vendor < ApplicationRecord
   validates :name, :description, :contact_name, :contact_phone, presence: true
-  validates :credit_accepted, inclusion: [true, false]
+  validate :credit_accepted_validation, on: [:create, :update]
 
   has_many :market_vendors
   has_many :markets, through: :market_vendors
+
+  private
+
+  def credit_accepted_validation
+    errors.add(:credit_accepted, "must be true or false") unless [true, false].include?(self[:credit_accepted])
+  end
 end
