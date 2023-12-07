4d3ac3db11b02b6c934a5c9ea11523357407b132
Xander Hendry <vhendry.coding@gmail.com>
2023-12-07 13:13:01 -0500

    refactor error handling to take advantage of inheritance

--- app/controllers/api/v0/markets_controller.rb
+++ app/controllers/api/v0/markets_controller.rb
@@ -1,21 +1,12 @@
 module Api
   module V0
     class MarketsController < ApplicationController
-      rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
-
       def index
         render json: MarketSerializer.new(Market.all)
       end
 
       def show
         render json: MarketSerializer.new(Market.find(params[:id]))
-      end
-
-      private
-
-      def not_found_response(exception)
-        render json: ErrorSerializer.new(ErrorMessage.new(exception.message, 404))
-                                    .serialize_json, status: :not_found
       end
     end
   end
--- app/controllers/api/v0/vendors_controller.rb
+++ app/controllers/api/v0/vendors_controller.rb
@@ -1,9 +1,6 @@
 module Api
   module V0
     class VendorsController < ApplicationController
-      rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
-      rescue_from ActiveRecord::RecordInvalid, with: :record_invalid_response
-
       def index
         render json: VendorSerializer.new(Market.find(params[:market_id]).vendors)
       end
@@ -31,14 +28,6 @@ module Api
 
       def vendor_params
         params.permit(:name, :description, :contact_name, :contact_phone, :credit_accepted)
-      end
-
-      def record_invalid_response(exception)
-        render json: ErrorSerializer.new(ErrorMessage.new(exception.message, 400)).serialize_json, status: :bad_request
-      end
-
-      def not_found_response(exception)
-        render json: ErrorSerializer.new(ErrorMessage.new(exception.message, 404)).serialize_json, status: :not_found
       end
     end
   end
