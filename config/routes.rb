406376933bcbadb08c454617f315c3ffdfcf2915
Xander Hendry <vhendry.coding@gmail.com>
2023-12-07 00:02:20 -0500

    added market vendor delete route

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
     end
   end
   post '/api/v0/market_vendors', to: 'api/v0/market_vendors#create'
+  delete '/api/v0/market_vendors', to: 'api/v0/market_vendors#delete'
 end
