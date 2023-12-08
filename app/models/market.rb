de7bf4bd9d1712b7dd3a40a11129bbcb35263a48
Xander Hendry <vhendry.coding@gmail.com>
2023-12-07 22:49:28 -0500

    refactor search_by_fragment to be case_insensitive

--- app/models/market.rb
+++ app/models/market.rb
@@ -7,19 +7,20 @@ class Market < ApplicationRecord
     query_params = query.keys.sort
     if query_params == ['city', 'name', 'state']
       #200?
-      results = Market.where(city: query['city']).and(Market.where(state: query['state'])).and(Market.where(name: query['name']))
+      results = Market.where('city ILIKE ?', "%#{query['city']}%").and(Market.where('state ILIKE ?', "%#{query['state']}%")).and(Market.where('name ILIKE ?', "%#{query['name']}%"))
+
     elsif query_params == ['name', 'state']
       # 200?
-      results = Market.where(state: query['state']).and(Market.where(name: query['name']))
+      results = Market.where('state ILIKE ?', "%#{query['state']}%").and(Market.where('name ILIKE ?', "%#{query['name']}%"))
     elsif query_params == ['city', 'state']
       # 200?
-      results = Market.where(city: query['city']).and(Market.where(state: query['state']))
+      results = Market.where('city ILIKE ?', "%#{query['city']}%").and(Market.where('state ILIKE ?', "%#{query['state']}%"))
     elsif query_params == ['state']
       # 200?
-      results = Market.where(state: query['state'])
+      results = Market.where('state ILIKE ?', "%#{query['state']}%")
     elsif query_params == ['name']
       # 200?
-      results = Market.where(name: query['name'])
+      results = Market.where('name ILIKE ?', "%#{query['name']}%")
     else 
       # 422?
       raise ActiveRecord::StatementInvalid, "Invalid set of parameters. Please provide a valid set of parameters to perform a search with this endpoint."
